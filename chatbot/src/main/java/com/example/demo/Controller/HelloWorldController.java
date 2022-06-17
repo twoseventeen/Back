c22da72b533af79d3e5c0dfe00f3b0dc672c8713
twoseventeen <101680348+twoseventeen@users.noreply.github.com>
2022-06-17 15:28:07 +0900

    .

--- chatbot/src/main/java/com/example/demo/Controller/HelloWorldController.java
+++ chatbot/src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -1,10 +1,10 @@
-package com.example.demo.controller;
+package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloWorldController {
 
     @GetMapping("/api/hello")
     public String test() {
