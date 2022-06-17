c7f87cd1b332544029a493764d776236a560d813
twoseventeen <101680348+twoseventeen@users.noreply.github.com>
2022-06-17 18:20:14 +0900

    .

--- chatbot/src/main/java/com/example/demo/ChatbotApplication.java
+++ chatbot/src/main/java/com/example/demo/ChatbotApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatbotApplication {
+public class chatbotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
+
+		SpringApplication.run(chatbotApplication.class, args);
 	}
 
 }
